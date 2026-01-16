abf566bdeac584458707a72923d4aae863685200
wendy nguyen <uyennhp134@gmail.com>
2026-01-16 09:45:57 -0700

    updated components folder, added siteheader

---
+++ app/components/SiteHeader.js
@@ -0,0 +1,15 @@
+export default function SiteHeader() {
+  return (
+    <header className="flex gap-4 justify-between py-4 px-8"> 
+        <div>Site Logo</div>
+        <nav>
+                <ul className= "flex gap-4">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        </nav>
+    
+    </header>
+    )
+}
